7d377d06860308ee2ed262b7e3eea60dc329178c
GDelaunay <goulwendelaunay@gmail.com>
2025-04-07 16:58:12 +0200

    Vue et Controller movies

---
+++ WebApplication1/Controllers/MovieController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+public class MovieController : Controller
+{
+    public IActionResult Index()
+    {
+        var movies = new List<Movie>
+        {
+            new Movie {Id = 1, Title = "Iron Man", Director = "Iron Man"},
+            new Movie {Id = 2, Title = "Matrix", Director = "The Dark"},
+            new Movie {Id = 3, Title = "The Dark", Director = "Nolan"},
+        };
+        return View(movies);
+    }
+}
