0b0812dd50cd204f3176aed428bf2e3eca05d91c
GDelaunay <goulwendelaunay@gmail.com>
2025-04-08 09:43:40 +0200

    Création controller et vue Book

---
+++ Exercice2/Controllers/BookController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Exercice2.Models;
+
+namespace Exercice2.Controllers;
+
+public class BookController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
