a98e526cff2568d57d601e50b935eb6788cf8082
GDelaunay <goulwendelaunay@gmail.com>
2025-04-08 10:05:44 +0200

    fin exercice 2

--- Exercice2/Controllers/BookController.cs
+++ Exercice2/Controllers/BookController.cs
@@ -7,6 +7,13 @@ public class BookController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        List<Book> books = new List<Book>
+        {
+            new Book{Id = 0, Title = "Le Petit Prince", Author = "Antoine de Saint-Exupéry", Year = 1998},
+            new Book{Id = 1, Title = "1984", Author = "George Orwell", Year = 1999},
+            new Book{Id = 2, Title = "L'Étranger", Author = "Author 1", Year = 2000},
+            new Book{Id = 3, Title = "Harry Potter à l'école des sorciers", Author = "J.K. Rowling", Year = 2001}
+        };
+        return View(books);
     }
 }
--- Exercice2/Models/Book.cs
+++ Exercice2/Models/Book.cs
@@ -5,4 +5,5 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; } = "";
     public string Author { get; set; } = "";
+    public int Year { get; set; }
 }
