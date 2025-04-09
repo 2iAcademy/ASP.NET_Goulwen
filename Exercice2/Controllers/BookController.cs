1d0b954860bde20369bdde75661d90b64f699466
GDelaunay <goulwendelaunay@gmail.com>
2025-04-09 10:28:49 +0200

    correction exercice 2

--- Exercice2/Controllers/BookController.cs
+++ Exercice2/Controllers/BookController.cs
@@ -5,12 +5,12 @@ namespace Exercice2.Controllers;
 
 public class BookController : Controller
 {
-    public List<Book> books = new List<Book>
+    private List<Book> books = new List<Book>
     {
-        new Book{Id = 0, Title = "Le Petit Prince", Author = "Antoine de Saint-Exupéry", Year = 1998},
-        new Book{Id = 1, Title = "1984", Author = "George Orwell", Year = 1999},
-        new Book{Id = 2, Title = "L'Étranger", Author = "Author 1", Year = 2000},
-        new Book{Id = 3, Title = "Harry Potter à l'école des sorciers", Author = "J.K. Rowling", Year = 2001}
+        new Book{Id = 1, Title = "Le Petit Prince", Author = "Antoine de Saint-Exupéry", Year = 1998},
+        new Book{Id = 2, Title = "1984", Author = "George Orwell", Year = 1999},
+        new Book{Id = 3, Title = "L'Étranger", Author = "Author 1", Year = 2000},
+        new Book{Id = 4, Title = "Harry Potter à l'école des sorciers", Author = "J.K. Rowling", Year = 2001}
     };
     
     public IActionResult Index()
