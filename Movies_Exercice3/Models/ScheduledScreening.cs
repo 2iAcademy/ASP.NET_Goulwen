1ae5aa9210e0b41e8243aa054b4ab77c99ee3038
GDelaunay <goulwendelaunay@gmail.com>
2025-04-10 15:25:14 +0200

    add end empty line

--- Movies_Exercice3/Models/ScheduledScreening.cs
+++ Movies_Exercice3/Models/ScheduledScreening.cs
@@ -4,7 +4,7 @@ public class ScheduledScreening
 {
     public int Id { get; set; }
     public required int MovieId { get; set; }
-    public required int ScreeningRoomId { get; set; }
+    public required int ScreenRoomId { get; set; }
     public required DateTime StartTime { get; set; }
     public int ReservationsCount { get; set; }
 }
