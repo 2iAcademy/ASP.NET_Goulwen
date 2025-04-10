b9041b90d4f36bf3a3e5e45851b820f29938fd39
GDelaunay <goulwendelaunay@gmail.com>
2025-04-10 09:54:56 +0200

    ajout persistance EF webapp1

--- WebApplication1/Controllers/MovieController.cs
+++ WebApplication1/Controllers/MovieController.cs
@@ -1,19 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
 using WebApplication1.Models;
 
 namespace WebApplication1.Controllers;
 
 public class MovieController : Controller
 {
-    public IActionResult Index()
+  private readonly AppDbContext _context;
+  public MovieController(AppDbContext context)
+  {
+    _context = context;
+  }
+  public IActionResult Index()
+  {
+    var movies = _context.Movie.ToList();
+    return View(movies);
+  }
+
+  public IActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public IActionResult Create(Movie movie)
+  {
+    if (ModelState.IsValid)
     {
-        var movies = new List<Movie>
-        {
-            new Movie {Id = 1, Title = "Iron Man", Director = "Iron Man"},
-            new Movie {Id = 2, Title = "Matrix", Director = "The Dark"},
-            new Movie {Id = 3, Title = "The Dark", Director = "Nolan"},
-        };
-        
-        return View(movies);
+      _context.Movie.Add(movie);
+      _context.SaveChanges();
+      return RedirectToAction(nameof(Index));
     }
+    return View(movie);
+  }
 }
