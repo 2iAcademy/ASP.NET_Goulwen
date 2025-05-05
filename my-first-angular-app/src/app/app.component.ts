d1bc0792906eb5924d93294840b585db889668b5
GDelaunay <goulwendelaunay@gmail.com>
2025-05-05 11:57:23 +0200

    premiers composants test

--- my-first-angular-app/src/app/app.component.ts
+++ my-first-angular-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HelloComponent} from './hello/hello.component';
+import {CounterComponent} from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloComponent],
+  imports: [RouterOutlet, HelloComponent, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
