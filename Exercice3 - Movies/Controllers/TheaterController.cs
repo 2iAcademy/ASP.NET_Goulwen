70de614301c3cfdd9390aee609c3d6b87cde1bd9
GDelaunay <goulwendelaunay@gmail.com>
2025-05-06 16:32:08 +0200

    Création API MovieController

---
+++ Exercice3 - Movies/Controllers/TheaterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
+using Movies_Exercice3.Data;
+
+namespace Movies_Exercice3.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TheaterController(AppDbContext context)
+{
+    private readonly AppDbContext _context = context;
+    private readonly JsonSerializerOptions _jsonOptions = new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+    
+    [HttpGet]
+    public string Get()
+    {
+        return JsonSerializer.Serialize(_context.Theater, _jsonOptions);
+    }
+    
+}
