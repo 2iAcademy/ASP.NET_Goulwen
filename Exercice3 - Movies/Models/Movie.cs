90f182aab9594afac93774d0fe1293e156fdb23f
GDelaunay <goulwendelaunay@gmail.com>
2025-06-04 14:38:08 +0200

    fix DateTime, URLS & docker compose

--- Exercice3 - Movies/Models/Movie.cs
+++ Exercice3 - Movies/Models/Movie.cs
@@ -5,7 +5,7 @@ public class Movie
     public int Id { get; set; }
     public required string Title { get; set; }
     public required List<Genre> Genres { get; set; }
-    public required DateOnly ReleaseDate { get; set; }
+    public required DateTime ReleaseDate { get; set; }
     public required Person Director { get; set; }
     public required List<Person> Actors { get; set; }
 }
--- Exercice3 - Movies/Models/Person.cs
+++ Exercice3 - Movies/Models/Person.cs
@@ -4,7 +4,7 @@ public class Person
 {
     public int Id { get; set; }
     public required string FullName { get; set; }
-    public DateOnly DateOfBirth { get; set; }
+    public DateTime DateOfBirth { get; set; }
     public List<Movie> MoviesDirected { get; set; }
     public List<Movie> MoviesPlayedIn { get; set; }
 }
